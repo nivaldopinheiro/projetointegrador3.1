8e34cb638aec44f969543efa19ab56f9e649d729
nivaldopinheiro <2004364@aluno.univesp.br>
2022-11-29 00:39:58 -0300

    Add cool face API

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ var app = express();
 var PORT = 3000;
 
 //carregado helps de logado 
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 const {eAdmin} = require("./helpers/eAdmin")
 
 //app.engine('handlebars' , handlebars.engine({defaultLayout : 'main'}))
